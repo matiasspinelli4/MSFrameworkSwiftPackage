e931cd1934715ff0979bad4d53ed28e820635a21
Matias Spinelli <matias.spinelli@wolox.com.ar>
2019-07-05 14:17:03 -0300

    Initial Commit

---
+++ MSFrameworkSwiftPackage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MSFrameworkSwiftPackage
+//
+//  Created by Matias Spinelli on 05/07/2019.
+//  Copyright © 2019 Matias Spinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
